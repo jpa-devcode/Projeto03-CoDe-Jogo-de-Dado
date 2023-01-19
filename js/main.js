7a51cf222e50c9482d3fabb9f81c65c65a76304c
Jezimiel Marcondes <jpaintodev@gmail.com>
2023-01-19 19:46:59 -0300

    Validando a entrada de números do jogador de 1 e 6

--- js/main.js
+++ js/main.js
@@ -1,41 +1,74 @@
-function jogarDado() {
-  var jogadaDoDado = Math.floor(Math.random() * 6 + 1);
-  var numeroDoJogador = document.getElementById('numero').value;
-  console.log(jogadaDoDado);
-  console.log(numeroDoJogador);
+var quantidadeDeJogadas = 0;
+var acertosDoJogador = 0;
 
-  if (numeroDoJogador == '') {
-    alert('Você não informou o número!');
-    return;
-  }
-
-  switch (jogadaDoDado) {
-    case 1:
-      document.getElementById('face').src = 'img/face_01.png';
-      break;
-    case 2:
-      document.getElementById('face').src = 'img/face_02.png';
-      break;
-    case 3:
-      document.getElementById('face').src = 'img/face_03.png';
-      break;
-    case 4:
-      document.getElementById('face').src = 'img/face_04.png';
-      break;
-    case 5:
-      document.getElementById('face').src = 'img/face_05.png';
-      break;
-    case 6:
-      document.getElementById('face').src = 'img/face_06.png';
-      break;
-  }
-
-  if (jogadaDoDado == numeroDoJogador) {
-    document.getElementById('comparacao').innerHTML = '&#61';
-  } else if (jogadaDoDado != numeroDoJogador && numeroDoJogador != '') {
-    document.getElementById('comparacao').innerHTML = '&#35';
-  }
-
-  document.getElementById('numero').value = '';
-  document.getElementById('numeroDoJogador').innerHTML = numeroDoJogador;
+function reiniciarJogo() {
+  window.location.reload(true);
 }
+
+document.getElementById('faceDoDado').style.animationPlayState = 'paused';
+
+function jogarDado(event) {
+  event.preventDefault();
+
+  if (quantidadeDeJogadas < 6) {
+    var jogadaDoDado = Math.floor(Math.random() * 6 + 1);
+    var numeroDoJogador = document.getElementById('numero').value;
+
+    if (numeroDoJogador == '') {
+      alert('Você não informou o número!');
+      return;
+    }
+
+    document.getElementById('faceDoDado').style.animationPlayState = 'running';
+
+    switch (jogadaDoDado) {
+      case 1:
+        document.getElementById('faceDoDado').src = 'img/face_01.png';
+        break;
+      case 2:
+        document.getElementById('faceDoDado').src = 'img/face_02.png';
+        break;
+      case 3:
+        document.getElementById('faceDoDado').src = 'img/face_03.png';
+        break;
+      case 4:
+        document.getElementById('faceDoDado').src = 'img/face_04.png';
+        break;
+      case 5:
+        document.getElementById('faceDoDado').src = 'img/face_05.png';
+        break;
+      case 6:
+        document.getElementById('faceDoDado').src = 'img/face_06.png';
+        break;
+    }
+
+    setTimeout(function () {
+      document.getElementById('faceDoDado').style.animationPlayState = 'paused';
+    }, 1000);
+
+    quantidadeDeJogadas += 1;
+
+    document.getElementById('quantidadeDeJogadas').innerHTML =
+      quantidadeDeJogadas;
+
+    if (jogadaDoDado == numeroDoJogador) {
+      acertosDoJogador += 1;
+      document.getElementById('numeroDeAcertos').innerHTML = acertosDoJogador;
+    }
+  }
+
+  if (quantidadeDeJogadas == 6) {
+    document.getElementById('jogarDado').style.cursor = 'default';
+    document.getElementById('jogarDado').style.backgroundColor = '#b4b4b4';
+    document.getElementById('jogarDado').style.color = '#000';
+    document.getElementById('jogarDado').style.borderColor = '#b4b4b4';
+  }
+
+  if (quantidadeDeJogadas == 6) {
+  }
+}
+
+document.getElementById('jogarDado').addEventListener('submit', jogarDado);
+document
+  .getElementById('reiniciarJogo')
+  .addEventListener('click', reiniciarJogo);
